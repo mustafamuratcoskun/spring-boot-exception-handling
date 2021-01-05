58e61684561527bc95ccdbe8df325a70b450be1b
Mustafa Murat Coşkun <coskun.m.murat@gmail.com>
2021-01-05 21:26:49 +0300

    Initial Configuration

---
+++ src/main/java/com/yazilimbilimi/springexceptionhandling/SpringExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.yazilimbilimi.springexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExceptionHandlingApplication.class, args);
+	}
+
+}
